0533b5e2e42fe8964f19c3814669ac0f0d99331b
jasonzhuang <bluezyg@gmail.com>
2013-08-13 22:39:48 +0800

    define filter as service

--- basic/filter/filter.js
+++ basic/filter/filter.js
@@ -3,10 +3,22 @@ var app = angular.module('myApp',[]);
 //Note: in th filter factory, we can't get the $scope
 app.filter('reverse',function(){
     return function(input, upper) {//second paramter is passed through reverse:true
-        //$scope.message can't receive'
         var out = input.split('').reverse().join('');
         if(upper) {
             out = out.toUpperCase();
+        }
+        return out;
+    }
+})
+
+//define filter as service, MUST name as xxxFilter, and in the html, use as XXX, NOT xxxFilter
+app.factory('oddFilter', function(){
+    return function(input){
+        var out = [];
+        for(var i=0;i<input.length; i++) {
+            if(i%2 == 0){
+                out.push(i);
+            }
         }
         return out;
     }
